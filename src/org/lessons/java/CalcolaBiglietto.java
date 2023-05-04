b2a57833da519ffe28d41086a872eb5a0e6ecb57
FlorianKomnino <florian.komnino@outlook.it>
2023-05-04 17:12:40 +0200

    completed first part of FizzBuzz exercise

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -4,34 +4,47 @@ import java.util.Scanner;
 
 public class CalcolaBiglietto {
 	public static void main(String[] args) {
-		float pricePerKm = 0.21f;
-		System.out.println(pricePerKm);
 		
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Please type the length of your trip in kilometers");
 		
-		String tripLength;
-		tripLength = in.nextLine();
-		Integer tripLengthInNumber = Integer.valueOf(tripLength);
+		int tripLength = in.nextInt();
 		
 		System.out.println("Please type your age");
 		
-		String userAge;
-		userAge = in.nextLine();
-		Integer userAgeInNumber = Integer.valueOf(userAge);
+		int userAge = in.nextInt();
 		
-		if (userAgeInNumber > 65) {
-			float tripPrice = (tripLengthInNumber * pricePerKm) / 100 * 60;
-			System.out.println("Your ticket price is : " + tripPrice);
-		} else if (userAgeInNumber < 18) {
-			float tripPrice = (tripLengthInNumber * pricePerKm) / 100 * 80;
-			System.out.println("Your ticket price is : " + tripPrice);
+		int totPrice = tripLength * 21;
+		
+		if (userAge > 65) {
+			totPrice = totPrice / 100 * 60;
+			
+			float finalPrice = totPrice / 100f;
+			System.out.println(
+					"final price: " 
+					+ String.format("%.2f", finalPrice)
+					+ "E"
+				);
+		} else if (userAge < 18) {
+			totPrice = totPrice / 100 * 80;
+			
+			float finalPrice = totPrice / 100f;
+			System.out.println(
+					"final price: " 
+					+ String.format("%.2f", finalPrice)
+					+ "E"
+				);
 		} else {
-			float tripPrice = tripLengthInNumber * pricePerKm;
-			System.out.println("Your ticket price is : " + tripPrice);
+			float finalPrice = totPrice / 100f;
+			System.out.println(
+					"final price: " 
+					+ String.format("%.2f", finalPrice)
+					+ "E"
+				);
 		}
+
+		in.close();	
 		
-		in.close();
 	}
 }
